dcac4493c0fb9b9dc88999c19460e521a1499476
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 13:33:05 +0530

    Created Bean for the product Details

--- src/main/java/com/niit/bej/bean/BeanConfig.java
+++ src/main/java/com/niit/bej/bean/BeanConfig.java
@@ -1,4 +1,13 @@
 package com.niit.bej.bean;
 
+import com.niit.bej.domain.Product;
+import org.springframework.context.annotation.Bean;
+
 public class BeanConfig {
+
+    @Bean
+    public Product productDetails() {
+        return new Product("A101", "Samsung-Galaxy S-22", 10, "Smart-Phone");
+    }
+
 }
