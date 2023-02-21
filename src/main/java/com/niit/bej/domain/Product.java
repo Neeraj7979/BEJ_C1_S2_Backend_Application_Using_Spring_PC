7d11f89d8a6cd55813838a5abfca3938021131cb
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 13:19:29 +0530

    Created properties for Product class

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -1,4 +1,9 @@
 package com.niit.bej.domain;
 
 public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+    private String productCategory;
+
 }
