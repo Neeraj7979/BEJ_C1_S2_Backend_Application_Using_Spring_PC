767fa1241a61b7b1812358afec8427395ac5dd5f
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 13:46:17 +0530

    Created  equals and hash code for Product order fields

--- src/main/java/com/niit/bej/domain/ProductOrder.java
+++ src/main/java/com/niit/bej/domain/ProductOrder.java
@@ -1,5 +1,7 @@
 package com.niit.bej.domain;
 
+import java.util.Objects;
+
 public class ProductOrder implements Order {
     private String orderId;
     private int orderQuantity;
@@ -36,5 +38,18 @@ public class ProductOrder implements Order {
 
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductOrder that = (ProductOrder) o;
+        return orderQuantity == that.orderQuantity && Double.compare(that.totalAmount, totalAmount) == 0 && Objects.equals(orderId, that.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, orderQuantity, totalAmount);
     }
 }
