7b9ef5b3106e9d0cdd8753913eebcbcf039fb5ee
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 14:03:52 +0530

    Created object of product order and call getbean method to get order details

--- src/main/java/com/niit/bej/App.java
+++ src/main/java/com/niit/bej/App.java
@@ -1,6 +1,7 @@
 package com.niit.bej;
 
 import com.niit.bej.bean.BeanConfig;
+import com.niit.bej.domain.ProductOrder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,5 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+        ProductOrder order1 = applicationContext.getBean(ProductOrder.class);
+
     }
 }
