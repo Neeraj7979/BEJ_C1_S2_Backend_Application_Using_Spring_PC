103ba3dfeb8b2fd3d8fae6945176a9bbec5a8ee8
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 13:22:18 +0530

    Created getter and setter for the product fields

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -15,4 +15,36 @@ public class Product {
 
     public Product() {
     }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
 }
