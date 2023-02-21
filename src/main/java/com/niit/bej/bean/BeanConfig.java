041937a92f679c00d98603ad2c7bf2daede84363
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-21 13:52:52 +0530

    Created bean for productOrder class

--- src/main/java/com/niit/bej/bean/BeanConfig.java
+++ src/main/java/com/niit/bej/bean/BeanConfig.java
@@ -1,6 +1,7 @@
 package com.niit.bej.bean;
 
 import com.niit.bej.domain.Product;
+import com.niit.bej.domain.ProductOrder;
 import org.springframework.context.annotation.Bean;
 
 public class BeanConfig {
@@ -8,6 +9,11 @@ public class BeanConfig {
     @Bean
     public Product productDetails() {
         return new Product("A101", "Samsung-Galaxy S-22", 10, "Smart-Phone");
+    }
+
+    @Bean
+    public ProductOrder printProductOrder() {
+        return new ProductOrder("P01", 5, 65000);
     }
 
 }
